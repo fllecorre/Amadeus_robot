c3df9b1b3fadbb128f00b00addf446b7822b3384
flecorre <florian.lecorre@amadeus.com>
2023-05-05 09:59:17 +0200

    precise cyberark lib version to use

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -13,7 +13,7 @@ curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
 
 pip3 install robotframework==6.0.2
 pip3 install requests==2.28.2
-pip install cyberark_lib -i https://repository.rnd.amadeus.net/api/pypi/pypi/simple
+pip install cyberark_lib==1.3 -i https://repository.rnd.amadeus.net/api/pypi/pypi/simple
 
 export PYTHONPATH="${PYTHONPATH}:/usr/local/lib/python3.9"
 export PYTHONPATH="${PYTHONPATH}:/usr/local/lib/python3.9/site-packages"
